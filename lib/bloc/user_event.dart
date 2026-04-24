2aac8e82f026ede8f6087ba9a00085b05fc4586e
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-04-24 11:01:40 +0700

    tambah event LoadUsers dan AddUserEvent pada UserEvent

---
+++ lib/bloc/user_event.dart
@@ -0,0 +1,10 @@
+import '../../domain/entities/user_entity.dart';
+
+abstract class UserEvent {}
+
+class LoadUsers extends UserEvent {}
+
+class AddUserEvent extends UserEvent {
+  final UserEntity user;
+  AddUserEvent(this.user);
+}
