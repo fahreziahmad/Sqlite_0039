3ba4bd329108656c737399c6128e87159e236966
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-04-24 11:14:10 +0700

    tambah UI form dan logic add/update user pada UserFormPage

--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -24,3 +24,52 @@ class _UserFormPageState extends State<UserFormPage> {
       _emailController.text = widget.user!.email;
     }
   }
+
+  @override
+  Widget build(BuildContext context) {
+    final isEdit = widget.user != null;
+
+    return Scaffold(
+      appBar: AppBar(title: Text(isEdit ? "Edit User" : "Tambah User")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _nameController,
+              decoration: const InputDecoration(labelText: "Nama Lengkap", border: OutlineInputBorder()),
+            ),
+            const SizedBox(height: 15),
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: "Email", border: OutlineInputBorder()),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+                  final newUser = UserEntity(
+                    id: isEdit ? widget.user!.id : DateTime.now().millisecondsSinceEpoch.toString(),
+                    name: _nameController.text,
+                    email: _emailController.text,
+                  );
+
+                  if (isEdit) {
+                    context.read<UserBloc>().add(UpdateUserEvent(newUser));
+                  } else {
+                    context.read<UserBloc>().add(AddUserEvent(newUser));
+                  }
+
+                  Navigator.pop(context);
+                },
+                child: Text(isEdit ? "Simpan Perubahan" : "Simpan User Baru"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
